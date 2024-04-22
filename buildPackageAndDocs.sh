895f48ed9bedfd4ebc9a1838b5bb9451b3685506
aletgn <alessandro.tognan@gmail.com>
2024-04-22 17:25:50 +0200

    restore previous config read the docs

--- buildPackageAndDocs.sh
+++ buildPackageAndDocs.sh
@@ -1,6 +1,6 @@
 pip3 uninstall bfade -y
 python3 -m build
-# pip3 install dist/b_fade-0.0.0-py3-none-any.whl
+pip3 install dist/b_fade-0.0.0-py3-none-any.whl
 sphinx-apidoc -o ./docs/ ./src/
 cd docs/
 make clean & make html
